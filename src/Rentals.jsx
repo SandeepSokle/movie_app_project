a1f427a1a8ca4768c8f7eabfd67dabff053decf3
Sandeep Sokle <60534028+SandeepSokle@users.noreply.github.com>
2021-09-06 14:14:47 +0530

    update

--- src/Rentals.jsx
+++ src/Rentals.jsx
@@ -1,5 +1,14 @@
+
+
+
+
+
 function Rentals() {
-    return <h1>Rentals</h1>;
+    return <h1>Note: For this coding example, you will need to install a Chrome extension called “CORS Unblock”, s
+o that you can use an external 3rd party API without running Cross Origin issues. 
+
+Install the Chrome extension here: 
+https://chrome.google.com/webstore/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino/related?hl=en</h1>;
   }
   
   export default Rentals;
